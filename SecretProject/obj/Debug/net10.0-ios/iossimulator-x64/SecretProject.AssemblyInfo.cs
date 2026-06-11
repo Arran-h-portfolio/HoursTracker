c2248724f016636a5bd6e22201adc469e061bc36
BVB-Glitch <207627199+Arran-h-portfolio@users.noreply.github.com>
2026-06-11 22:36:58 +0100

    changes from setting up the application

--- SecretProject/obj/Debug/net10.0-ios/iossimulator-x64/SecretProject.AssemblyInfo.cs
+++ SecretProject/obj/Debug/net10.0-ios/iossimulator-x64/SecretProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SecretProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+f1ffa77ea756711c4a92cb76fc2d310ca3fced02")]
 [assembly: System.Reflection.AssemblyProductAttribute("SecretProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SecretProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
